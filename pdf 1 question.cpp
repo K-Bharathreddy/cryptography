8e5d9ca1613f20cddd08d9f810a25d3bb712e2db
K-Bharathreddy <112238525+K-Bharathreddy@users.noreply.github.com>
2024-03-27 08:37:47 +0530

    Add files via upload

---
+++ pdf 1 question.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+void encryptCaesar(char *message, int key) {
+    while (*message) {
+        if (isalpha(*message)) {
+            char base = isupper(*message) ? 'A' : 'a';
+            *message = (char)(((int)(*message - base + key) % 26) + base);
+        }
+        message++;
+    }
+} 
+void decryptCaesar(char *message, int key) {
+    encryptCaesar(message, 26 - key); 
+}
+
+int main() {
+    char message[100];
+    int key;
+    printf("Enter a message: ");
+    fgets(message, sizeof(message), stdin);
+    printf("Enter the key (a number between 0 and 25): ");
+    scanf("%d", &key);
+    encryptCaesar(message, key);
+    printf("Encrypted message: %s\n", message);
+    decryptCaesar(message, key);
+    printf("Decrypted message: %s\n", message);
+
+    return 0;
+}
